c7e9be9e0ac839d4227b59c88d5a0af9c23c7717
basilico2013 <juanbasilico2@gmail.com>
2022-06-08 23:25:15 -0300

    avance

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -62,41 +62,41 @@ function sonIguales(x, y) {if(x==y){return true};return false
   
 }
 
-function tienenMismaLongitud(str1, str2) {
+function tienenMismaLongitud(str1, str2) {function tienenMismaLongitud(str1, str2) {if (str1.length==str2.length) {return true}; return false;}
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
   
 }
 
-function menosQueNoventa(num) {
+function menosQueNoventa(num) {if (num<90){return true};return false;
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
   
 }
 
-function mayorQueCincuenta(num) {
+function mayorQueCincuenta(num) {if (num>50){return true}; return false;
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
   
 }
 
-function obtenerResto(x, y) {
+function obtenerResto(x, y) {var resultado= x%y; return resultado
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
   
 }
 
-function esPar(num) {
+function esPar(num) {if (num%2==0){return true}; return false
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
   
 }
 
-function esImpar(num) {
+function esImpar(num) {if (num%2>0){return true}; return false
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
